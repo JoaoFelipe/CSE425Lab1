f64fe2efc5c6e563ebb88a8df70e46d935f2cdbf
Joao Felipe Nicolaci Pimentel <jfn1@shell.cec.wustl.edu>
2012-10-05 20:13:06 -0500

    split for labels, numbers, parenthesis, commas, separators and others

--- dbfunc.scm
+++ dbfunc.scm
@@ -10,8 +10,10 @@
 ))
 
 
-; This function splits a string by space with auxiliar params
-(define split (lambda (str pos curr lis)
+; This function splits a string by space, label(sequence of alphabetic characters), 
+; number(sequence of numeric characters), parenthesis('(' or ')'), comma(','), and('^'), separator(':-'), and other
+; with auxiliar params
+(define split (lambda (str pos curr lis status)
 	(cond 
 		((>= pos (string-length str)) 
 			(if (= (string-length curr) 0)
@@ -21,14 +23,110 @@
 		)
 		(else
 			(let ((ch (string-ref str pos))) 
-				(if (char-whitespace? ch) 
-					(split str (+ pos 1) "" 
-						(if (= (string-length curr) 0)
-							lis
-							(cons curr lis)
+				(cond
+					((char-whitespace? ch) 
+						(split str (+ pos 1) "" 
+							(if (= (string-length curr) 0)
+								lis
+								(cons curr lis)
+							)
+							""
 						)
 					)
-					(split str (+ pos 1) (string-append curr (string ch)) lis)
+					((and (char-numeric? ch) (not (string=? status "numeric")))
+						(split str pos "" 
+							(if (= (string-length curr) 0)
+								lis
+								(cons curr lis)
+							)
+							"numeric"
+						)
+					)
+					((and (char-alphabetic? ch) (not (string=? status "alphabetic")))
+						(split str pos "" 
+							(if (= (string-length curr) 0)
+								lis
+								(cons curr lis)
+							)
+							"alphabetic"
+						)
+					)
+					((char=? ch #\^)
+						(split str (+ pos 1) "" 
+							(cons "^" 
+								(if (= (string-length curr) 0)
+									lis
+									(cons curr lis)
+								)
+							)
+							""
+						)
+					)
+					((char=? ch #\()
+						(split str (+ pos 1) "" 
+							(cons "(" 
+								(if (= (string-length curr) 0)
+									lis
+									(cons curr lis)
+								)
+							)
+							""
+						)
+					)
+					((char=? ch #\))
+						(split str (+ pos 1) "" 
+							(cons ")" 
+								(if (= (string-length curr) 0)
+									lis
+									(cons curr lis)
+								)
+							)
+							""
+						)
+					)
+					((char=? ch #\,)
+						(split str (+ pos 1) "" 
+							(cons "," 
+								(if (= (string-length curr) 0)
+									lis
+									(cons curr lis)
+								)
+							)
+							""
+						)
+					) 
+					((and (char=? ch #\:) (< (+ pos 1) (string-length str)) (char=? (string-ref str (+ pos 1)) #\-))
+						(split str (+ pos 2) "" 
+							(cons ":-" 
+								(if (= (string-length curr) 0)
+									lis
+									(cons curr lis)
+								)
+							)
+							""
+						)
+					) 
+					((and (string=? status "numeric") (not (char-numeric? ch)))
+						(split str (+ pos 1) (string ch) 
+							(if (= (string-length curr) 0)
+								lis
+								(cons curr lis)
+							)
+							"other"
+						)
+					)
+					((and (string=? status "alphabetic") (not (char-alphabetic? ch)))
+						(split str (+ pos 1) (string ch) 
+							(if (= (string-length curr) 0)
+								lis
+								(cons curr lis)
+							)
+							"other"
+						)
+					)
+					(else
+						(split str (+ pos 1) (string-append curr (string ch)) lis status)
+					)
 				)
 			)
 			
@@ -37,9 +135,12 @@
 ))
 
 
-; This function splits a string by space 
+; This function splits a string by space, label(sequence of alphabetic characters), 
+; number(sequence of numeric characters), parenthesis('(' or ')'), comma(','), and('^'), separator(':-'), and other
+; Eg.: "my ancestor(x,y):-parent(x,z)" may be separated into:
+;    ["my" "ancestor" "(" "x" "," "y" ")" ":-" "parent" "(" "x" "," "z" ")"]
 (define split-string (lambda (str)
-	(reverse (split str 0 "" '()))
+	(reverse (split str 0 "" '() ""))
 ))
 
 
